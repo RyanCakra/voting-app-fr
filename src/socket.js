4b82e516980669b5bde90d321f1307b7ea09a63a
RyanCakra <104733198+RyanCakra@users.noreply.github.com>
2025-06-29 00:36:49 +0700

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,3 @@
 // socket.js
 import { io } from 'socket.io-client';
-export const socket = io(import.meta.env.VITE_API_URL || 'http://localhost:3001');
+export const socket = io(import.meta.env.VITE_SOCKET_URL || 'http://localhost:3001');
